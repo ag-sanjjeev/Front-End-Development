44dc0a314dd195fa03b7030a9feec2de78d753cd
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2024-09-26 17:22:51 +0530

    Added Login Successful Page

---
+++ pages-and-themes/login-successful-page/main.js
@@ -0,0 +1,35 @@
+/*
+ ________________________________________________
+(            Login Successful Page             ()
+\-----------------------------------------------\
+|                                               |
+|   Copyright 2024 ag-sanjjeev                  |
+|                                               |
+|-----------------------------------------------|
+|   The source code is licensed under           |
+|   MIT-style License.                          |
+|                                               |
+|-----------------------------------------------|
+|                                               |
+|   The usage, permission and condition         |
+|   are applicable to this source code          |
+|   as per license.                             |
+|                                               |
+|-----------------------------------------------|
+|                                               |
+|   That can be found in LICENSE file           |
+|   or at https://opensource.org/licenses/MIT.  |
+(_______________________________________________(
+
+*/
+
+// Document ready event
+$(document).ready(function() {
+	// Animating message words
+	$('#message-word span:first-child').css('animation-delay', '2s');
+	$('#message-word span:last-child').css('animation-delay', '2.2s');
+	// call after timeout
+	setTimeout(function() {		
+		$('#icon').removeClass('fa-lock').removeClass('text-danger').addClass('text-success').addClass('fa-unlock-alt');
+	}, 1600);
+});
