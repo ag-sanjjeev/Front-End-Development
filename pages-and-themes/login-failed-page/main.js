bd08a5d739eeaf56cdaca53ea1802716d829b399
ag-sanjjeev <91313054+ag-sanjjeev@users.noreply.github.com>
2024-09-27 09:53:27 +0530

    Added Login Failed Page

---
+++ pages-and-themes/login-failed-page/main.js
@@ -0,0 +1,31 @@
+/*
+ ________________________________________________
+(               Login Failed Page              ()
+\-----------------------------------------------\
+|                                               |
+|   Copyright 2024 ag-sanjjeev                  |
+|                                               |
+|-----------------------------------------------|
+|   The source code is licensed under           |
+|   MIT-style License.                          |
+|                                               |
+|-----------------------------------------------|
+|                                               |
+|   The usage, permission and condition         |
+|   are applicable to this source code          |
+|   as per license.                             |
+|                                               |
+|-----------------------------------------------|
+|                                               |
+|   That can be found in LICENSE file           |
+|   or at https://opensource.org/licenses/MIT.  |
+(_______________________________________________(
+
+*/
+
+// Document ready event
+$(document).ready(function() {
+	// Animating message words
+	$('#message-word span:first-child').css('animation-delay', '2s');
+	$('#message-word span:last-child').css('animation-delay', '2.2s');
+});
